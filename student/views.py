80507ed6bfde4f3b4f47fd8b3b89b94258b56b60
wuchuny <1204721935@qq.com>
2019-12-30 19:10:31 +0800

    Try 1

---
+++ student/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from student.forms import StudentForm
+from student.models import Student
+
+
+def index(request):
+    students = Student.objects.all()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            #student.status=0
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = StudentForm()
+
+    context = {
+         'students':students,
+         'form':form,
+    }
+    return render(request,'index.html',context=context)
